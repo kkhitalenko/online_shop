e1a7d8613fffd43987a65c645ce58c1495bf7894
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-10-29 17:16:52 +0300

    create models and configure the admin site

--- backend/goods/admin.py
+++ backend/goods/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Good, Price, Type
+
+
+@admin.register(Good)
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'qty', 'date_update', 'type']
+    search_fields = ['title']
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'value', 'currency', 'good']
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
--- backend/goods/models.py
+++ backend/goods/models.py
@@ -1,3 +1,50 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Good(models.Model):
+    title = models.CharField('название', max_length=64)
+    price = models.OneToOneField('Price', on_delete=models.CASCADE,
+                                 verbose_name='цена', related_name='good')
+    qty = models.PositiveIntegerField('количество')
+    barcode = models.CharField('штрихкод', max_length=64, unique=True)
+    date_update = models.DateField('дата обновления', auto_now=True)
+    type = models.ForeignKey('Type', on_delete=models.CASCADE,
+                             verbose_name='тип', related_name='goods')
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+    def __str__(self):
+        return self.title
+
+
+class Price(models.Model):
+    CURRENCIES = {
+        'RUB': 'RUB',
+        'USD': 'USD',
+        'EUR': 'EUR',
+    }
+    currency = models.CharField('валюта', max_length=3, choices=CURRENCIES)
+    value = models.DecimalField('стоимость', max_digits=8, decimal_places=2,
+                                validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = 'цена'
+        verbose_name_plural = 'цены'
+
+    def __str__(self):
+        return f'{self.value} {self.currency}'
+
+
+class Type(models.Model):
+    title = models.CharField('название', max_length=64, unique=True)
+    description = models.TextField('описание')
+
+    class Meta:
+        verbose_name = 'тип товара'
+        verbose_name_plural = 'типы товаров'
+
+    def __str__(self):
+        return self.title
